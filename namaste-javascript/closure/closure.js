6bb4876b612d84b5193b4da423320c50ad810bab
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-05-05 10:38:56 +0530

    closures

---
+++ namaste-javascript/closure/closure.js
@@ -0,0 +1,15 @@
+function outerFunction() {
+    const outerVariable = "Hello, ";
+
+    function innerFunction(name) {
+        const innerVariable = "Welcome to my website!";
+        console.log(`${outerVariable} ${name} ${innerVariable}`);
+    }
+
+    return innerFunction;
+}
+
+// const fun = outerFunction();
+// fun("Sanjeev")
+let a = null;
+console.log(a)
