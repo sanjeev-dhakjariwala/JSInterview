8531f001f499608fc19e8d35ed8f972495b0c53a
Sanjeev Jha <sanjeevplacement2020@gmail.com>
2023-10-24 20:44:31 +0530

    Interview Questions

---
+++ lydiahallie/tictactoe.js
@@ -0,0 +1,19 @@
+const board = [
+    ['0','X','.'],
+    ['.','.','.'],
+    ['.','.','.']
+]
+// 0 X
+
+function validation(player1, player2, position1, position2){
+    //validation
+    winning()
+}
+function winning(player1, player2){
+    //winning logic
+    return playerName
+}
+function input(){
+    position1 = []
+    
+}
