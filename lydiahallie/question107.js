a78572f007c84978676a6077afd4bd665d02f8d0
Sanjeev Jha <sanjeevplacement2020@gmail.com>
2024-02-10 11:10:55 +0530

    q107 updated

--- lydiahallie/question107.js
+++ lydiahallie/question107.js
@@ -1,1 +1,7 @@
 JSON.parse();
+/**
+ * A: Parses JSON to a JavaScript value
+ * B: Parses a JavaScript object to JSON
+ * C: Parses any JavaScript value to JSON
+ * D: Parses JSON to a JavaScript object only
+ */
